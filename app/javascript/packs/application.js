bd6fcb1553165450b2823459a2372271790c6db3
crazyah <a27328996@hotmail.com>
2020-05-03 14:49:27 +0800

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("styles")
-
-
+import 'bootstrap/'
+$(document).on('turbolinks:load', function(){
+  $("#show_modal_btn").click(function(){
+    console.log("I will open modal");
+    $("#myModal").modal("show");
+  });
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
