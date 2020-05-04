1166ad67f83419f0e4a9dddda60492022fd71e07
lynnchang <tv725987@gmail.com>
2020-05-04 21:53:46 +0800

    issue #34 HTTParty install + 打 API + 將取回的資料存入資料庫 WIP

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,35 @@
+class ArticleController < ApplicationController 
+  def create
+    response = HTTParty.get("https://extractorapi.com/api/v1/extractor/?apikey=e3e6d4d35cbf7ecc564ed3d42fca87a75cc242dc&url=#{url_params[:link]}")
+    response_hash = JSON.parse(response.body)
+
+    # @article = Article.new(url: url_params)
+    # @article.title = response_hash["title"]
+    # @article.content = response_hash["text"]
+    # @article.link = response_hash["domain"]
+    # @article.
+
+    if response.code == 200
+      @article.assign_attributes({
+        user_id: 1,   #改 current_user 
+        link: url_params[:link],
+        title: response_hash['title'],
+        content: response_hash['text']
+      })
+      @article.save
+      @article.errors.full_messages
+      render json: @article.to_json
+      #render something 到 mylist 頁面
+    else
+      # alert: '請輸入正確網址'
+    end
+
+  end
+
+
+
+  private
+  def url_params
+    params.require(:article).permit(:link) 
+  end
+end
