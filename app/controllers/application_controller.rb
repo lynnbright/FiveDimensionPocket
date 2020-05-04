c05cf5f3840a9bc8b3414e11e365d4ce3cfbdb91
lynnchang <tv725987@gmail.com>
2020-05-04 21:53:46 +0800

    issue #34 HTTParty install + 打 API + 將取回的資料存入資料庫 WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :article_new
   layout :layout_by_resource
 
   def layout_by_resource
@@ -6,4 +7,12 @@ class ApplicationController < ActionController::Base
       "for_user"
     end
   end
+  
+
+  private
+  def article_new
+    @article = current_user ? current_user.articles.new : Article.new
+  end
+
+  #補登入 check 
 end
