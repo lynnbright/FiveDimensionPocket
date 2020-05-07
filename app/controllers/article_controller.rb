64e9b550291d2ceaa2078827a76beabd86d3d751
lynnchang <tv725987@gmail.com>
2020-05-07 17:28:44 +0800

    issue #34 可上傳單張圖片至 active_storage

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,17 +1,13 @@
 class ArticleController < ApplicationController
 
   def index
+    # @articles_json = current_user.articles.to_json
+    @articles = Article.with_attached_article_cover   #解決 N+1 問題
   end
 
   def create
     response = HTTParty.get("https://extractorapi.com/api/v1/extractor/?apikey=e3e6d4d35cbf7ecc564ed3d42fca87a75cc242dc&url=#{url_params[:link]}")
     response_hash = JSON.parse(response.body)
-
-    # @article = Article.new(url: url_params)
-    # @article.title = response_hash["title"]
-    # @article.content = response_hash["text"]
-    # @article.link = response_hash["domain"]
-    # @article.
 
     if response.code == 200
       @article.assign_attributes({
@@ -19,20 +15,20 @@ class ArticleController < ApplicationController
         link: url_params[:link],
         title: response_hash['title'],
         content: response_hash['text'],
-        domain: response_hash['domain']
+        domain: response_hash['domain'],
       })
       @article.save
+      #render 之前要先把 input value 清空
       render :index
     else
       flash[:notice] = '請輸入正確網址'
     end
-
   end
 
 
 
   private
   def url_params
-    params.require(:article).permit(:link) 
+    params.require(:article).permit(:link, :article_cover) 
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,18 @@
+require 'open-uri'
+
 class Article < ApplicationRecord
   has_many :article_tags
   has_many :tags, through :article_tags
   default_scope { where.not(created_at: nil).order(id: :desc)}
   belongs_to :user
+
+  #圖片儲存
+  has_one_attached :article_cover
+  before_save :grab_cover
+
+  private
+  def grab_cover
+    file = open("https://womany.net/cdn-cgi/image/w=800,fit=scale-down,f=auto/https://castle.womany.net/images/content/pictures/62832/womany_161114_4496_6_EhMdP_1503483761-16149-5365.jpg") 
+    self.article_cover.attach(io: file, filename: 'cover.jpg', content_type: 'image/jpg')
+  end
 end
