cdf4f1622f7d73ff59c234893e267eabc8ade6c9
Joey <aileen60222@gmail.com>
2020-05-10 16:26:02 +0800

    install stimulus

--- app/javascript/controllers/tag_controller.js
+++ app/javascript/controllers/tag_controller.js
@@ -1,20 +1,28 @@
 import { Controller } from "stimulus"
-import $ from 'jquery'
 
 export default class extends Controller {
-  static targets = [ "tagList" ]
+  // static targets = [ "id" ]
+  static html='<select name="select2-multiple" multiple="multiple" class="form-control"></select>'
 
-  connect(){
-    Swal.fire(
-      'The Internet?',
-      'That thing is still around?',
-      'question'
-    )
+  connect(){    
+    
   }
   editTag(e) {
-
+    Swal.fire({
+      title: "title",
+      html: html,
+      showCancelButton: true,
+      showConfirmButton: true,
+      onOpen: function (dObj) {
+        $('[name="select2-multiple"]').select2({
+            tags: true,
+            tokenSeparators: [',', ' '],
+            width: '100%',
+            dropdownParent: $(".swal2-modal")
+        });
+      }
+    });     
+   
   }
 
-  openModal(e) {
-  }
 }
