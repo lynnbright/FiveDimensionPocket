cfa7fd1861750a76e7c138600123d3cb4f9304e2
Joey <aileen60222@gmail.com>
2020-05-10 18:59:23 +0800

    install axios

--- app/javascript/controllers/tag_controller.js
+++ app/javascript/controllers/tag_controller.js
@@ -1,26 +1,35 @@
 import { Controller } from "stimulus"
+import axios from 'axios'
+
 
 export default class extends Controller {
-  // static targets = [ "tagSelect" ]
+  static targets = [ "articleId" ]
  
   connect(){    
 
   }
   editTag(e) {
+    let id = this.articleIdTarget.value
     Swal.fire({
-      title: "title",
+      title: "新增標籤",
       html: document.querySelector('#tagSelect').innerHTML,
       showCancelButton: true,
       showConfirmButton: true,
-      onOpen: function (dObj) {
+      onOpen: function () {
         $('[name="select2-multiple"]').select2({
             tags: true,
             tokenSeparators: [',', ' '],
             width: '100%',
             dropdownParent: $(".swal2-modal")
-        });
+        });        
+      },
+    }).then((result) => {
+      if(result.value) {
+        axios.post(`api/article/#{id}/tags`)
+             .then((res) => { console.table(res.data) })
+             .catch((error) => { console.error(error) })
       }
-    });     
+    })    
   }
 
 }
