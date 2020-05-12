3628fb84e17c7656615cc70ef30d3160889dfa80
lynnchang <tv725987@gmail.com>
2020-05-12 15:26:42 +0800

    issue #98 複製連結功能 WIP

--- app/javascript/articles_sharebtn/index.js
+++ app/javascript/articles_sharebtn/index.js
@@ -3,7 +3,26 @@ import './application.scss'
 $(document).on('turbolinks:load', function() {
   $('#card-wrap .js-collapse-btn').on('click', function(e){
     e.preventDefault();
-    // $(this).siblings('.shared-bar:eq(0)').toggleClass('show')
-    $('.shared-bar').toggleClass('show')
+    $(this).siblings('.shared-bar:eq(0)').toggleClass('show')
+    // $('.shared-bar').toggleClass('show')
+  })
+
+  $('.shared-conect-btn').on('click', function(e){
+    e.preventDefault();
+      let url = document.querySelector('.copylink').value;
+      console.log(url)
+      copyToClip(url)
   })
 })
+
+
+function copyToClip(str) {
+  function listener(e) {
+    e.clipboardData.setData("text/html", str);
+    e.clipboardData.setData("text/plain", str);
+    e.preventDefault();
+  }
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+};
