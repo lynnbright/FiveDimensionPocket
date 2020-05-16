4c2207c663445a3ebe15e7513104a29d22e3e0ba
lynnchang <tv725987@gmail.com>
2020-05-17 02:30:06 +0800

    issue #30 標題搜尋製作 WIP

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,11 @@ require("styles")
 require("articles_sharebtn")
 
 import $ from 'jquery'
-window.$ = $
+window.$ = window.jquery = $
 
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 import "@fortawesome/fontawesome-free/js/all"
 
@@ -28,9 +29,27 @@ window.Chart = Chart
 import "controllers"
 $(document).on('turbolinks:load', function(){
 
+  //text-to-speech player 顯示
   $('#js-create-speech').click(function() {
     let $this = $(this);
     $this.removeClass('d-flex').addClass('d-none');
     $this.siblings('.loading').removeClass('d-none').addClass('d-inline-block')
   });
+
+
+  //標題搜尋
+  $('#test-input').on('change paste keyup', function(){
+      let value = $(this).val();
+      if(!value){
+        // $('.title-search-style').html('');
+        $('.title-search-style').html('');
+      }
+      else{
+        searchCandidate(value); 
+      }
+      
+      function searchCandidate(value){
+        $('.title-search-style').html('.container .cards-area .card-title-search:not([data-title *="' + value + '"]) { display: none }');
+      }
+    })
 });
