e113906dcbc868b007dc90ea84d499c11282217e
Aweiisacat <awei@LAPTOP-14H3JOK8.localdomain>
2020-05-21 13:55:57 +0800

    修改大小寫檔名以及搬運檔案

---
+++ app/javascript/script/navbar.js
@@ -0,0 +1,29 @@
+$('document').ready( () => {
+  $('.plus').click( () => {
+      $('.plus-block').removeClass('displaynone')
+  })
+  $('.search').click( () => {
+    $('.search-block').removeClass('displaynone')
+  })
+  $('.plus-close').click( e => {
+    e.stopPropagation();
+    $('.plus-block').addClass('displaynone')
+  })
+  $('.search-close').click( e => {
+    e.stopPropagation();
+    $('.search-block').addClass('displaynone')
+  })
+  $('.burger').click( () => {
+    $('.root-side-bar').removeClass('displaynone')
+  })
+  $('.user').click( () => {
+    $('.user-list').toggleClass('displaynone')
+  })
+  $('.user-list-block').click( e => {
+    e.stopPropagation();
+  })
+  $('.root-side-bar-close').click( () => {
+    $('.root-side-bar').addClass('displaynone')
+  })
+})
+
