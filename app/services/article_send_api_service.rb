676216893867dd657f035354197e31c4c8ba4e50
lynnchang <tv725987@gmail.com>
2020-05-22 22:22:25 +0800

    1.將 index&search&favorite 版面與新 navbar & sidebar & card 樣式合板、連結調整為 link_to    2.新增文章失敗條件判斷（若 text 為空值，則再打一次 api）WIP

--- app/services/article_send_api_service.rb
+++ app/services/article_send_api_service.rb
@@ -4,9 +4,11 @@ class ArticleSendApiService
     @article_url = article_url
   end
 
-  def perform
-    response = HTTParty.get("https://extractorapi.com/api/v1/extractor/?apikey=e3e6d4d35cbf7ecc564ed3d42fca87a75cc242dc&url=#{@article_url}&fields=domain,title,author,date_published,images,videos,clean_html,html")
+  def perform(retry_times = 0)
+    response = HTTParty.get("https://extractorapi.com/api/v1/extractor/?apikey=e3e6d4d35cbf7ecc564ed3d42fca87a75cc242dc&url=#{@article_url}&fields=domain,title,date_published,images,videos,clean_html,html")
     response_hash = JSON.parse(response.body)
+
+    # raise 'No Text!' if response_hash['text'].blank?
     @short_description = response_hash['text'].split('').first(50).join('')
     
     #萃取出 og:image 圖片位址
@@ -17,11 +19,22 @@ class ArticleSendApiService
     @clean_html = response_hash['clean_html'].gsub!(/\"/, '\'') || 'null'
     @clean_content = @clean_html.match(/<p[^>]*>[\w|\W]*<\/i>/).to_s
     
-    return {
-            success: response.code == 200, 
-            data: response_hash,
-            extract_data: 
-            { clean_html: @clean_html, clean_content: @clean_content, ogimage_address: @ogimage_address, short_description: @short_description }
-           }
+    {
+      success: true, 
+      data: response_hash,
+      extract_data: {
+        clean_html: @clean_html,
+        clean_content: @clean_content,
+        ogimage_address: @ogimage_address,
+        short_description: @short_description 
+      }
+    }
+  rescue
+    if retry_times < 1
+      retry_times += 1
+      retry
+    else
+      { success: false }
+    end
   end
 end
