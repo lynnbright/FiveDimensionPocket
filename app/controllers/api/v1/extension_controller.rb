1052fd368cf18f6b362f00ff8397a151ec7dc71a
Aweiisacat <face992302@gmail.com>
2020-05-26 14:50:45 +0800

    新增一些回報給前端

--- app/controllers/api/v1/extension_controller.rb
+++ app/controllers/api/v1/extension_controller.rb
@@ -34,11 +34,13 @@ class Api::V1::ExtensionController < ApplicationController
           clean_content: result[:extract_data][:clean_content],
           short_description: result[:extract_data][:short_description],
         })
+        render json: {message: '儲存成功!'}, status: 200
         @article.save
       else
-        render json: {message: '重新試一次'}, status: 401
+        render json: {message: '此網站無法存取'}, status: 401
       end
     else
+      render json: {message: '此網站儲存過'}, status: 200
       check_article_exist.update(created_at: Time.now)
     end
   end
