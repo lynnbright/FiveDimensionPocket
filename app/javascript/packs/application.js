8344894b93762719059c692dd2086a08c9e3589c
Joey <aileen60222@gmail.com>
2020-05-26 16:57:38 +0800

    整理css引用

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("styles")
+require("scripts")
 require("articles_sharebtn")
 
 import $ from 'jquery'
@@ -10,7 +11,7 @@ window.$ = window.jquery = $
 
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "../scripts/sb-admin-2.min.js"
+
 
 import "@fortawesome/fontawesome-free/js/all"
 
