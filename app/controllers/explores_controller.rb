4408091ae9c061812c2f4ef511dcfc8ddf84a6a5
Joey <aileen60222@gmail.com>
2020-05-28 14:17:27 +0800

    優化探索區的code

--- app/controllers/explores_controller.rb
+++ app/controllers/explores_controller.rb
@@ -1,22 +1,22 @@
 class ExploresController < ApplicationController
-  before_action :authenticate_user!, :find_user_has_three_public_articles
+  before_action :authenticate_user!, :user_has_three_public_articles
   
   def index  
-    @Users =  User.includes(:user_last_articles, :articles, :follow_lists).where(id: @Users)
+    @users = User.includes(:user_last_articles, :articles, :follow_lists).where(id: @explore_user_id)
   end
 
   def following
-    # 找出所有公開超過三篇且被追蹤的
-    @Users = @Users.select{ |u_id| current_user.follow_lists.find_by(following_id: u_id) }
-    @Users =  User.includes(:user_last_articles, :articles, :follow_lists).where(id: @Users)
+    # 篩出所有公開超過三篇且被追蹤的id
+    @following_user_id = @explore_user_id.select{ |u_id| current_user.follow_lists.find_by(following_id: u_id) }
+    @users = User.includes(:user_last_articles, :articles, :follow_lists).where(id: @following_user_id)
 
     render :index 
   end
 
   private
 
-  def find_user_has_three_public_articles  
+  def user_has_three_public_articles  
     # 分組之後不能用欄位直接排序，只能找其欄位值特徵做排序(最大或最小等等)
-    @Users = UserLastArticle.group(:user_id).order('MAX(created_at) DESC').having('COUNT(user_id) > 2').limit(3).count.keys
+    @explore_user_id = UserLastArticle.group(:user_id).order('MAX(created_at) DESC').having('COUNT(user_id) > 2').limit(3).count.keys
   end
 end
