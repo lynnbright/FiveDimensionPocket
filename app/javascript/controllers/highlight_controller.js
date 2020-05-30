69873883f45248daeb4befe95ca25507f267d977
Joey <aileen60222@gmail.com>
2020-05-30 11:58:36 +0800

    #167 WIP 標記刪除功能，標記fun搬出來

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -6,15 +6,41 @@ function placeTooltip(x_pos, y_pos, type) {
   // 增加一個後面可以用來判斷的，name或其他
   var d = document.getElementById('tooltip');
   if (type === "new") {
-    d.setAttribute  = 'btn newHighlight'
+    d.setAttribute("data-action", "highlight#delHighlight");
     d.innerHTML = `<i class="fas fa-pen"></i>HighLight`;
   } else {
-    d.className  = 'btn delHighlight'
+    d.setAttribute("data-action", "highlight#delHighlight");
     d.innerHTML = `<i class="far fa-trash-alt"></i>刪除`;
   }
   d.style.position = "absolute";
   d.style.left = x_pos + 'px';
   d.style.top = (y_pos - 40) + 'px';
+}
+
+function highlightRange(range, counter, id) {
+  // 如何把特定的ele的位置抓出來
+  let newNode = document.createElement("span");
+  newNode.classList.add('highlight');
+  newNode.id = (`highlight${counter}`);
+  range.surroundContents(newNode);
+  let el = document.getElementById(`highlight${counter}`).parentElement;
+  let index = [].indexOf.call(el.parentElement.children, el);
+  let myData = {
+    content: document.getElementById(`highlight${counter}`).innerText,
+    element_id: `highlight${counter}`,
+    paragraph_index: index
+  }
+  Rails.ajax({
+    url: `/api/v1/articles/${id}/highlight`,
+    type: 'POST',
+    data: new URLSearchParams(myData).toString(),
+    success: resp => {
+      // console.log(resp)
+    },
+    error: err => {
+      console.log(err);
+    }
+  })
 }
 
 export default class extends Controller {
@@ -69,40 +95,14 @@ export default class extends Controller {
       if (counter > 3) {
         Swal.fire('一般會員只能標記三筆喔')
       } else {
-        highlightRange(selRange, counter++);
+        highlightRange(selRange, counter++, id);
       }
       document.getSelection().removeAllRanges()
       document.getElementById("tooltip").classList.add('notDisplayed');
       document.getElementById("tooltip").style = '';
     }, false);
 
-    function highlightRange(range, counter) {
-      // 如何把特定的ele的位置抓出來
-      let newNode = document.createElement("span");
-      newNode.classList.add('highlight');
-      newNode.id = (`highlight${counter}`);
-      range.surroundContents(newNode);
-      let el = document.getElementById(`highlight${counter}`).parentElement;
-      let index = [].indexOf.call(el.parentElement.children, el);
-      let myData = {
-        content: document.getElementById(`highlight${counter}`).innerText,
-        element_id: `highlight${counter}`,
-        paragraph_index: index
-      }
-      console.log(el.parentElement.children)
-      console.log(myData)
-      Rails.ajax({
-        url: `/api/v1/articles/${id}/highlight`,
-        type: 'POST',
-        data: new URLSearchParams(myData).toString(),
-        success: resp => {
-          // console.log(resp)
-        },
-        error: err => {
-          console.log(err);
-        }
-      })
-    }
+
     // 參考資料:https://stackoverflow.com/questions/25695212/jquery-how-to-tag-selected-text-using-textbox-as-tooltip
   }
   showTooltip(e) {
@@ -113,5 +113,11 @@ export default class extends Controller {
   hideTooltip() {
     document.getElementById("tooltip").classList.remove('notDisplayed');
     document.getElementById("tooltip").style = '';
+  }
+  addHighlight(){
+
+  }
+  delHighlight(){
+    
   }
 }
