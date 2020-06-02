3e5b0e7cab533899a02ae8a926141832bbad4013
lynnchang <tv725987@gmail.com>
2020-06-03 01:17:46 +0800

    更新 extension_controller @user.id + params[:url] code

--- app/controllers/api/v1/extension_controller.rb
+++ app/controllers/api/v1/extension_controller.rb
@@ -32,11 +32,11 @@ class Api::V1::ExtensionController < ApplicationController
         })
         @article.save
         render json: {message: '儲存成功!'}, status: 200
-        
+
       elsif result[:nokogiri_success] == 'nokogiri_success'
         @article.assign_attributes({
-          user_id: current_user.id,
-          link: url_params[:link],
+          user_id: @user.id,
+          link: params[:url],
           title: result[:extract_data][:title],
           content: result[:extract_data][:content],
           domain: result[:extract_data][:domain],
