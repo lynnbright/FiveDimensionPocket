c2420e3d7db95b4aaebcdd4bddaebf1b57aa948e
Aweiisacat <43492184+Aweiisacat@users.noreply.github.com>
2020-06-04 14:21:29 +0800

    Revert "Awei/extension fix"

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module FiveDimensionPocket
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options], credentials: false
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
   end
