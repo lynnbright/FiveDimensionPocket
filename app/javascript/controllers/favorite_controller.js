d9775b22f9a37358c37d66abe2adf13c55161bc4
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-06-08 00:15:09 +0800

    #193 WIP 文章內頁 navbar 的已讀及我的最愛，選取樣式調整

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 
 export default class extends Controller {
-  static targets = [ "articleId", "btn" ]
+  static targets = [ "articleId", "btn", "icon"]
 
   heart(e){
     e.preventDefault();
@@ -24,6 +24,33 @@ export default class extends Controller {
             } else{
               this.btnTarget.classList.add('bg-gray-500');
               this.btnTarget.classList.remove('hearted');
+            }
+          },
+         )
+         .catch ((error) => {
+          console.log(error);
+        } )
+  }
+  navHeart(e){
+    e.preventDefault();
+    
+    let articleId = this.articleIdTarget.value 
+    axios.post(`/api/v1/articles/${articleId}/favorite`)
+         .then((response) => {
+          let result = response.data.status
+            if (result === 'favorited'){   
+              this.iconTarget.classList.add('shake');
+              let self = this;
+              setTimeout(function () {
+                self.iconTarget.classList.remove('shake');
+              }, 500);
+              this.iconTarget.classList.add('fas');
+              this.iconTarget.classList.add('pink');
+              this.iconTarget.classList.remove('far');
+            } else{
+              this.iconTarget.classList.add('far');
+              this.iconTarget.classList.remove('fas');
+              this.iconTarget.classList.remove('pink');
             }
           },
          )
