90588d387228a63dccba0c6507ff45c301c9fddd
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2020-06-10 16:50:11 +0800

    修正探索頁bug

--- app/models/user_last_article.rb
+++ app/models/user_last_article.rb
@@ -4,6 +4,6 @@ class UserLastArticle < ApplicationRecord
 
   def self.explore_users 
     # 分組之後不能用欄位直接排序，只能找其欄位值特徵做排序(最大或最小等等)
-    self.group(:user_id).order('MAX(created_at) DESC').having('COUNT(user_id) > 2').limit(3).count.keys
+    self.group(:user_id).order('MAX(created_at) DESC').having('COUNT(user_id) > 2').count.keys
   end
 end
