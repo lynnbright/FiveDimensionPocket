3a0d989c8c382847cd2ff5532a524e614b83c821
lynnchang <tv725987@gmail.com>
2020-06-11 22:53:06 +0800

    刪除不必要的 code

--- app/javascript/articles_sharebtn/index.js
+++ app/javascript/articles_sharebtn/index.js
@@ -4,14 +4,8 @@ $(document).on('turbolinks:load', function() {
   $('#card-wrap .js-collapse-btn').on('click', function(e){
     e.preventDefault();
     $(this).siblings('.shared-bar:eq(0)').toggleClass('show')
-    // $('.shared-bar').toggleClass('show')
   })
 
-  // $('.shared-conect-btn').on('click', function(e){
-  //   e.preventDefault();
-  //   let url = $(this).parent().find('.copylink:eq(0)').val()
-
-  // })
 
   $('.shared-conect-btn').on('click', function(e){
     e.preventDefault();
@@ -22,15 +16,3 @@ $(document).on('turbolinks:load', function() {
     document.execCommand('copy');
   })
 })
-
-
-// function copyToClip(str) {
-//   function listener(e) {
-//     e.clipboardData.setData("text/html", str);
-//     e.clipboardData.setData("text/plain", str);
-//     e.preventDefault();
-//   }
-//   document.addEventListener("copy", listener);
-//   document.execCommand("copy");
-//   document.removeEventListener("copy", listener);
-// };
